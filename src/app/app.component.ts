d55ffbdc99ab31f5982ebc8c8c41fdd930b0f250
Huyen059 <samihuyen059@gmail.com>
2020-09-08 17:48:20 +0200

    can search for friend using email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Friend} from './friend';
 import {AddFriendService} from './add-friend.service';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {HttpHeaders} from '@angular/common/http';
+import {URLSearchParams} from 'url';
 
 @Component({
   selector: 'app-root',
@@ -13,19 +14,21 @@ export class AppComponent implements OnInit {
   title = 'project-name';
   languages = ['HTML', 'CSS', 'Javascript', 'PHP', 'Java', 'Python'];
   friendModel = new Friend('', '', '', '', '');
+  searchTerm = '';
   allFriends: any = [];
+  searchFriend: object;
 
   constructor(private addFriendService: AddFriendService, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getAllFriends('http://localhost:9000/allFriends');
+    // this.searchFriendByEmail('http://localhost:9000/findFriend?email=tim.broos@becode.org');
   }
 
   addFriend(): void {
-    // console.log(this.friendModel);
     this.addFriendService.addFriend(this.friendModel)
       .subscribe((data: Friend) => {
-      this.getAllFriends('http://localhost:9000/allFriends');
+        this.getAllFriends('http://localhost:9000/allFriends');
     });
   }
 
@@ -38,8 +41,24 @@ export class AppComponent implements OnInit {
     return await this.http.get(url, httpOptions).toPromise()
       .then(data => {
         this.allFriends = data;
-        // console.log(this.allFriends);
       });
+  }
+
+  async searchFriendByEmail(): Promise<any> {
+    let url = 'http://localhost:9000/findFriend?email=' + this.searchTerm;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    };
+    return await this.http.get(url, httpOptions).toPromise().then(friend => {
+      this.searchFriend = friend;
+      console.log(friend);
+    });
+  }
+
+  sanitizePhone(inputNumber: string): string {
+    return  inputNumber.replace(/[^+0-9]/g, '');
   }
 }
 
