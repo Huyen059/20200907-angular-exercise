ac3b486e67333f0ac36f2d8a55f7b5d8622c1484
Huyen059 <samihuyen059@gmail.com>
2020-09-08 12:13:28 +0200

    can display friend list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Friend} from './friend';
 import {AddFriendService} from './add-friend.service';
+import {HttpClient} from '@angular/common/http';
+import {HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'project-name';
   languages = ['HTML', 'CSS', 'Javascript', 'PHP', 'Java', 'Python'];
   friendModel = new Friend('', '', '', '', '');
-  constructor(private addFriendService: AddFriendService) {}
+  allFriends: any = [];
 
-// tslint:disable-next-line:typedef
-  addFriend() {
+  constructor(private addFriendService: AddFriendService, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getAllFriends('http://localhost:9000/allFriends');
+  }
+
+  addFriend(): void {
     // console.log(this.friendModel);
     this.addFriendService.addFriend(this.friendModel)
       .subscribe((data: Friend) => {
-      console.log(data);
+      this.getAllFriends('http://localhost:9000/allFriends');
     });
+  }
+
+  async getAllFriends(url: string): Promise<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    };
+    return await this.http.get(url, httpOptions).toPromise()
+      .then(data => {
+        this.allFriends = data;
+        // console.log(this.allFriends);
+      });
   }
 }
 
