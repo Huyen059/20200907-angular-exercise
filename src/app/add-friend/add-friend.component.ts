928b7d4e6a71505105a88f00e762cd176fa8eb51
Huyen059 <samihuyen059@gmail.com>
2020-09-09 16:37:29 +0200

    redirect to another route

--- src/app/add-friend/add-friend.component.ts
+++ src/app/add-friend/add-friend.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Friend} from '../friend';
 import {FriendsService} from '../friends.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-friend',
@@ -11,14 +12,14 @@ export class AddFriendComponent implements OnInit {
   languages = ['HTML', 'CSS', 'Javascript', 'PHP', 'Java', 'Python'];
   friendModel = new Friend('', '', '', '', '');
 
-  constructor(private friendsService: FriendsService) { }
+  constructor(private friendsService: FriendsService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   addFriend(): void {
     this.friendsService.add(this.friendModel)
-      .subscribe();
+      .subscribe(() => this.router.navigate(['/friend-list']));
   }
 
 }
